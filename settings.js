490cd7e4957f13b1d9dc506d03242e0777fc691d
CTK-WARRIOR <dbdandmore2020@gmail.com>
2022-01-13 16:36:02 +0530

    changes

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
     /* Browser path, can get by pasting this in url section "about:support", then find "Profile Folder", copy that and paste it here, aslso replace all "\" with "\\" */
-    "browser_path": "C:\\Users\\Example\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\0n0h14lp.default-release-1641851519690",
+    "browser_path": "C:\\Users\\RIshabh Yadav\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\0n0h14lp.default-release-1641851519690",
     /*Your School/College group name */
-    "group_name": "Tesr", 
+    "group_name": "Class Xl PCM group 21-22", 
     /* After how much time, it should join meeting */
     "join_meeting_after": 2, 
     /* After how many minutes it should check student counts (for the purpose of leave) */
